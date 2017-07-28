a2e3d15beff93b2539447180185e667aabd1c7e3
Tiago Almeida <tiago.b.almeida@gmail.com>
2017-07-28 18:33:39 +0100

    Increased default font size

--- init.el
+++ init.el
@@ -181,16 +181,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(case-fold-search t)
+ '(cua-mode t nil (cua-base))
  '(global-linum-mode t)
  '(inhibit-startup-screen t)
- '(js2-bounce-indent-p t))
+ '(js-indent-level 2)
+ '(js2-bounce-indent-p t)
+ '(tab-width 2))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
 
 
 ;; Trying to get tabs to work ffs

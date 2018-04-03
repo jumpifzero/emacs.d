9f83772cc52218e2b1853d97136b38a4cce2fb26
Tiago Almeida <tiago.b.almeida@gmail.com>
2018-04-03 15:14:21 +0100

    Changes

--- init.el
+++ init.el
@@ -20,8 +20,8 @@
 (add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
 (package-initialize)
 (global-company-mode)
-;(add-hook 'after-init-hook 'global-company-mode)
-
+                                        ;(add-hook 'after-init-hook 'global-company-mode)
+	
 ;;
 (use-package autopair
   :ensure t
@@ -44,6 +44,7 @@
 (use-package elpy :ensure t)
 (use-package monokai-theme :ensure t)
 (load "~/.emacs.d/markdown.el")
+(load "~/.emacs.d/typescript-config.el")
 (use-package multiple-cursors :ensure t)
 (use-package pabbrev :ensure t)
 (use-package realgud :ensure t)
@@ -56,13 +57,13 @@
 (use-package json-mode :ensure t)
 (use-package exec-path-from-shell :ensure t)
 
-;(use-package simple-httpd :ensure t)
-;(use-package skewer-mode :ensure t)
-;(add-hook 'js2-mode-hook 'skewer-mode)
-;(use-package auto-complete :ensure t)
-;(ac-config-default)
-;(use-package ac-js2 :ensure t)
-;(add-hook 'js2-mode-hook 'ac-js2-mode)
+                                        ;(use-package simple-httpd :ensure t)
+                                        ;(use-package skewer-mode :ensure t)
+                                        ;(add-hook 'js2-mode-hook 'skewer-mode)
+                                        ;(use-package auto-complete :ensure t)
+                                        ;(ac-config-default)
+                                        ;(use-package ac-js2 :ensure t)
+                                        ;(add-hook 'js2-mode-hook 'ac-js2-mode)
 
 ;; Cua Mode - for ctrl+c/ctrl+v windows style.
 (cua-mode t)
@@ -122,7 +123,7 @@
 (require 'yasnippet)
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets"              ;; personal snippets
-	"~/lib/yasnippet-snippets"         ;; the default collection
+        "~/lib/yasnippet-snippets"         ;; the default collection
         ))
 (yas-global-mode t)
 
@@ -132,8 +133,8 @@
 ;; JAVASCRIPT
 (add-hook 'javascript-mode-hook 'show-paren-mode)
 ;; js2-mode (javascript editing)
-;(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-;(add-to-list 'auto-mode-alist '("\\.js\\'" . pabbrev-mode))
+                                        ;(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+                                        ;(add-to-list 'auto-mode-alist '("\\.js\\'" . pabbrev-mode))
 
 ;; Do not word wrap by default
 (setq-default truncate-lines 1)
@@ -145,95 +146,101 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Key Bindings                                                              ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Undo is Ctrl+Z
-(global-unset-key "\C-z")
-(global-set-key "\C-z" 'undo)
-;; Ctrl+a is select all, not move-beggining-of-line
-(global-unset-key "\C-a")
-(global-set-key "\C-a" 'mark-whole-buffer)
-;; Search is Ctrl+F
-(global-set-key (kbd "C-f") 'isearch-forward)
-(define-key isearch-mode-map "\C-f" 'isearch-repeat-forward)
-;; multiple-cursors. Select like this with Alt+f3
-(global-set-key (kbd "M-<f3>") 'mc/mark-all-like-this)
-;; Duplicate line is C-d. By default C-d is delete char
-(global-set-key (kbd "C-d") 'duplicate-line)
-;; Ctrl+S saves current file. Not ISearch - disabled as the default doesnt bother me
-;; (global-unset-key "\C-s")
-;; (global-set-key (kbd "C-s") 'save-buffer)
-;; Ctrl+k deletes current line
-(global-unset-key "\C-k")
-(global-set-key (kbd "C-k") 'my-delete-line)
-;; Ctrl+backspace deletes backward word
-(global-unset-key (kbd "<C-backspace>") )
-(global-set-key (kbd "<C-backspace>") 'my-backward-delete-word)
-;; Ctrl+(shift)tab changes between tabs
-(global-set-key (kbd "<C-iso-lefttab>") 'tabbar-backward-tab)
-(global-set-key (kbd "<C-tab>") 'tabbar-forward-tab)
-;; Ctrl+f4 - kill buffer
-(global-set-key (kbd "<C-f4>") 'kill-this-buffer)
+(let* ((customized-bindings t))
+  (if customized-bindings
+      (progn 
+        ;; Undo is Ctrl+Z
+        (global-unset-key "\C-z")
+        (global-set-key "\C-z" 'undo)
+        ;; Ctrl+a is select all, not move-beggining-of-line
+        (global-unset-key "\C-a")
+        (global-set-key "\C-a" 'mark-whole-buffer)
+        ;; Search is Ctrl+F
+        (global-set-key (kbd "C-f") 'isearch-forward)
+        (define-key isearch-mode-map "\C-f" 'isearch-repeat-forward)
+        ;; multiple-cursors. Select like this with Alt+f3
+        (global-set-key (kbd "M-<f3>") 'mc/mark-all-like-this)
+        ;; Duplicate line is C-d. By default C-d is delete char
+        (global-set-key (kbd "C-d") 'duplicate-line)
+        ;; Ctrl+S saves current file. Not ISearch - disabled as the default doesnt bother me
+        ;; (global-unset-key "\C-s")
+        ;; (global-set-key (kbd "C-s") 'save-buffer)
+        ;; Ctrl+k deletes current line
+        (global-unset-key "\C-k")
+        (global-set-key (kbd "C-k") 'my-delete-line)
+        ;; Ctrl+backspace deletes backward word
+        (global-unset-key (kbd "<C-backspace>") )
+        (global-set-key (kbd "<C-backspace>") 'my-backward-delete-word)
+        ;; Ctrl+(shift)tab changes between tabs
+        (global-set-key (kbd "<C-iso-lefttab>") 'tabbar-backward-tab)
+        (global-set-key (kbd "<C-tab>") 'tabbar-forward-tab)
+        ;; Ctrl+f4 - kill buffer
+        (global-set-key (kbd "<C-f4>") 'kill-this-buffer)    
+        )
+    )
+)
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Arguable defaults                                                         ;;
+  ;; Arguable defaults                                                         ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Do not generate backup files -
-;; http://emacsredux.com/blog/2013/05/09/keep-backup-and-auto-save-files-out-of-the-way/
-(setq make-backup-files nil)
+  ;; Do not generate backup files -
+  ;; http://emacsredux.com/blog/2013/05/09/keep-backup-and-auto-save-files-out-of-the-way/
+  (setq make-backup-files nil)
 
-;; auto revert mode - Refreshes files and non files if they changed on disk
-(global-auto-revert-mode 1)
-(setq global-auto-revert-non-file-buffers t)
+  ;; auto revert mode - Refreshes files and non files if they changed on disk
+  (global-auto-revert-mode 1)
+  (setq global-auto-revert-non-file-buffers t)
 
-;; View column numbers
-(setq column-number-mode t)
+  ;; View column numbers
+  (setq column-number-mode t)
 
-;; Enable Predictive Abbreviation everywhere FIXME-This isnt working
-(pabbrev-mode)
+  ;; Enable Predictive Abbreviation everywhere FIXME-This isnt working
+  (pabbrev-mode)
 
-;; Search is case insensitive by default. Press M-c to toggle
-(setq case-fold-search nil)
-(setq-default indent-tabs-mode nil) ; do not mix tabs and spaces
+  ;; Search is case insensitive by default. Press M-c to toggle
+  (setq case-fold-search nil)
+  (setq-default indent-tabs-mode nil) ; do not mix tabs and spaces
 
-;;
-(setq x-stretch-cursor t) ;makes cursor lager when over tabs
-(setq-default cursor-type 'bar)
-(setq-default tab-width 4)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(case-fold-search t)
- '(cua-mode t nil (cua-base))
- '(dired-recursive-deletes (quote always))
- '(global-linum-mode t)
- '(helm-command-prefix-key "C-;")
- '(inhibit-startup-screen t)
- '(js-indent-level 4)
- '(js2-bounce-indent-p t)
- '(python-indent-offset 4)
- '(tab-width 4))
+  ;;
+  (setq x-stretch-cursor t) ;makes cursor lager when over tabs
+  (setq-default cursor-type 'bar)
+  (setq-default tab-width 4)
+  (custom-set-variables
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(case-fold-search t)
+   '(cua-mode t nil (cua-base))
+   '(dired-recursive-deletes (quote always))
+   '(global-linum-mode t)
+   '(helm-command-prefix-key "C-;")
+   '(inhibit-startup-screen t)
+   '(js-indent-level 4)
+   '(js2-bounce-indent-p t)
+   '(python-indent-offset 4)
+   '(tab-width 4))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
-
-
-;; Trying to get tabs to work ffs
-;; https://stackoverflow.com/questions/344966/improved-tab-in-emacs
-;(defvar just-tab-keymap (make-sparse-keymap) "Keymap for just-tab-mode")
-;(define-minor-mode just-tab-mode
-;  "Just want the TAB key to be a TAB"
-;  :global t :lighter " TAB" :init-value 0 :keymap just-tab-keymap
-;  (define-key just-tab-keymap (kbd "TAB") 'indent-for-tab-command))
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
 
 
+  ;; Trying to get tabs to work ffs
+  ;; https://stackoverflow.com/questions/344966/improved-tab-in-emacs
+                                        ;(defvar just-tab-keymap (make-sparse-keymap) "Keymap for just-tab-mode")
+                                        ;(define-minor-mode just-tab-mode
+                                        ;  "Just want the TAB key to be a TAB"
+                                        ;  :global t :lighter " TAB" :init-value 0 :keymap just-tab-keymap
+                                        ;  (define-key just-tab-keymap (kbd "TAB") 'indent-for-tab-command))
 
-								
-; XML PRETTY PRINT
-; 'm not sure how sgml-mode might have changed over time. Today, I invoked C-x C-f foo.xml, M-x sgml-mode, then M-x sgml-pretty-print and my xml file got pretty printed. (Well, emacs hanged for twenty seconds or more before completing. It was a one line file before the pretty print and 720 lines after.) – daveloyall Aug 10 '15 at 18:14
+
+
+
+                                        ; XML PRETTY PRINT
+                                        ; 'm not sure how sgml-mode might have changed over time. Today, I invoked C-x C-f foo.xml, M-x sgml-mode, then M-x sgml-pretty-print and my xml file got pretty printed. (Well, emacs hanged for twenty seconds or more before completing. It was a one line file before the pretty print and 720 lines after.) – daveloyall Aug 10 '15 at 18:14
--- keys.md
+++ keys.md
@@ -1,3 +1,7 @@
+# Definitions
+Frame = Collection of windows
+Window = displays a buffer (file)
+
 C-v	Move forward one screenful
 M-v	Move backward one screenful
 C-l	Clear screen and redisplay all the text,
@@ -18,4 +22,51 @@ C-u x cmd prefix argument; repeats cmd x times or use x as an argument
 C-w   	  kill (cut)
 c-y	  yank (paste)
 
-line 498 tut
+# Find and replace
+
+M-x replace-string str newstr	Replaces str with newstr going forward.
+
+# Margins / Justification
+
+M-x auto-fill-mode turns on a mode which automatically breaks at word boundaries.
+You can set the margin width with C-x f number
+After making changes in the middle of a paragraph, type M-q to re-fill it.
+
+# Searching
+C-s	forward incremental search
+C-r	reverse incremental search
+During search, another press of C-S finds the next occurrence. Backspace finds the previous. Enter ends the search.
+
+# Scrolling
+
+C-M-v (control+alt+v) allows scrolling another window.
+
+# Saving
+
+C-x s	Save all buffers
+C-x C-x Save current buffer
+
+# Getting help
+
+C-h c (keys)	Which command is executed by the keys.
+C-h k (keys)	More documentation
+C-h f 		Docu about a function
+C-h v		Docu about a variable.
+M-x apropos	Same as C-h a
+C-h a str	Finds commands with str in name		
+C-h i 		Manuals
+
+
+# Documentation
+
+C-h m	Documentation for current mode.
+
+# Buffers
+
+C-x b	  Jump to buffer (with name)
+C-x C-b	  List of buffers
+C-x 1	  Maximize current window
+
+# Frames
+
+M-x make-frame (creates a new OS Window)
